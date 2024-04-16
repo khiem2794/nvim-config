26e42001d9a475b8c94060c8a2e3ff34730b0a42
Khiem <khiem2794@gmail.com>
2024-04-17 04:13:55 +0700

    Update lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,10 +3,14 @@ return {
   opts = function(_, opts)
     vim.list_extend(opts.ensure_installed, {
       "stylua",
+      "selene",
       "shfmt",
       "shellcheck",
       "luacheck",
       "rust-analyzer",
+      "tailwindcss-languague-server",
+      "typescript-language-server",
+      "css-lsp",
     })
   end,
 }
