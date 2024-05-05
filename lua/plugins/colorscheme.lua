13acc31636c70fa5118f45b2e9f00e3cbcd1e8a4
khiem2794 <khiem2794@gmail.com>
2024-05-05 19:02:51 +0700

    add neovide cfg + change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     { "wittyjudge/gruvbox-material.nvim" },
     { "LazyVim/LazyVim", opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "tokyonight",
     } },
   },
 }
